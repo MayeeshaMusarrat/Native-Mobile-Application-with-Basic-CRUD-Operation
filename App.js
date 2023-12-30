d9d2157a85f3cc7b8081a231c8f0fb663d7d947e
MayeeshaMusarrat <musarratmayeesha@gmail.com>
2023-12-31 03:27:32 +0600

    Init

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+const Stack = createNativeStackNavigator();
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import SourceFile from "./screens/SourceFile";
 
-export default function App() {
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text, Pressable, TouchableOpacity } from "react-native";
+
+const App = () => {
+  const [hideSplashScreen, setHideSplashScreen] = React.useState(true);
+
+  const [fontsLoaded, error] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer>
+        {hideSplashScreen ? (
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen
+              name="SourceFile"
+              component={SourceFile}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        ) : null}
+      </NavigationContainer>
+    </>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
+export default App;
---
+++ GlobalStyles.js
@@ -0,0 +1,22 @@
+/* fonts */
+export const FontFamily = {
+    robotoRegular: "Roboto-Regular",
+    robotoBold: "Roboto-Bold",
+    robotoMedium: "Roboto-Medium",
+  };
+  /* font sizes */
+  export const FontSize = {
+    size_sm: 14,
+    size_xs: 12,
+  };
+  /* Colors */
+  export const Color = {
+    colorWhite: "#fff",
+    colorDarkslateblue_100: "#11325d",
+    blue: "#3c7eee",
+  };
+  /* border radiuses */
+  export const Border = {
+    br_3xl: 22,
+  };
+  
