96464b06e564ea3368ec71a685f8b83cca5d49c2
MayeeshaMusarrat <musarratmayeesha@gmail.com>
2023-12-31 18:42:04 +0600

    Secured dotenv, used MongoClient

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,34 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const router = require('./routes/router')
-const mongoose = require('mongoose')
-require('dotenv/config')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+require('dotenv/config');
 
-const app =  express()
+const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const corsOptions = {
     origin: '*',
     credentials: true,
     optionSuccessStatus: 200
-}
+};
 
-app.use(cors(corsOptions))
-app.use('/', router)
+app.use(cors(corsOptions));
+const client = new MongoClient(process.env.DB_URI);
 
-mongoose.connect(process.env.DB_URI)
-  .then(() => console.log('DB Connected!'))
-  .catch(err => console.error('DB Connection Error:', err));
+client.connect()
+    .then(() => {
+        console.log('DB Connected!');
+    })
+    .catch((err) => {
+        console.error('DB Connection Error:', err);
+    });
 
-const port = process.env.PORT || 4000
+const database = client.db("testerDB"); 
+
+const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+    console.log(`Server is running on port ${port}`);
+});
