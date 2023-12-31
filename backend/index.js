de8b7d59023d1d91f4f8e716e9abf6b9bc681b02
MayeeshaMusarrat <musarratmayeesha@gmail.com>
2023-12-31 20:31:15 +0600

    Fetched Collections

--- backend/index.js
+++ backend/index.js
@@ -16,17 +16,29 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+
 const client = new MongoClient(process.env.DB_URI);
 
 client.connect()
     .then(() => {
         console.log('DB Connected!');
+        const collection = client.db("testerDB").collection("users");
+
+        collection.find({}).toArray()
+            .then((users) => {
+                console.log('Users in the "users" collection:', users);
+            })
+            .catch((err) => {
+                console.error('Error fetching users:', err);
+            })
+            .finally(() => {
+                client.close();
+                console.log('Connection closed');
+            });
     })
     .catch((err) => {
         console.error('DB Connection Error:', err);
     });
-
-const database = client.db("testerDB"); 
 
 const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
